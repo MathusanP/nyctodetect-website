1505b477aef2434d387338767a0dc280142852c4
agnag <mathuyt141@gmail.com>
2023-04-17 09:42:45 +0100

    feat(index.html): Added teachable machine ai

--- public/script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function showPopup() {
-    var container = document.getElementById("container")
-    container.style.visibility = "visible";
-}
-function removePopup() {
-    var container = document.getElementById("container")
-    container.style.visibility = "hidden";
-}
-
